f6b1668fc4ecafdfda141966157f70625bf2b833
Muyizereberissa <muyizereberissa22@gmail.com>
2024-02-14 10:12:37 +0200

    react

--- App.js
+++ App.js
@@ -3,10 +3,7 @@ import { StyleSheet, Text, View, TouchableOpacity, TextInput,} from 'react-nativ
 import Icon from 'react-native-vector-icons/FontAwesome'
 import Feather from 'react-native-vector-icons/Feather'
 import AntDesign from 'react-native-vector-icons/AntDesign'
-
-
-
-
+import { CheckBox } from "react-native-elements";
 
 
 export default function App() {
@@ -15,25 +12,30 @@ export default function App() {
       <Text style={{marginTop: 100, fontWeight: 'bold', fontSize: 30}}> Sign In</Text>
       <Text> Wellcome back! Please enter your details</Text>
       <StatusBar style="auto" />
-      <View style={{marginTop: 60}}>
-        <View style={styles.Feather}> 
-          <Feather name="mail" size={30} color="pink" />
-        </View>
-        <TouchableOpacity>
-        <Text  style={{borderWidth:1,marginTop:20,textAlign:'center', borderRadius:10,width:300,padding:15}}>b.hello@gmail.com</Text>
-        </TouchableOpacity>
+      <View style={{marginTop: 70, display: 'flex', flexDirection: 'column', alignItems: 'center',width:"100%"}}>
+      <View style={{display: 'flex', flexDirection: 'row', gap: 10, width: 300, borderWidth: 1, borderColor: '#EFECEC', borderRadius: 10, padding: 15, marginTop: 20,}}>
+       
+            <Feather name="mail" size={30} color="pink" marginLeft="10" />
+        
+        
+          <TextInput value="b.x@y.com"></TextInput>
+      </View>
+        <View style={{border: 'none',marginTop:20, borderRadius:10,width:300,padding:18, backgroundColor:'#fafbfc'}}>
+          <AntDesign name="lock" size={30} color="#81a1d4" />
+         
+            <TextInput  secureTextEntry placeholder="Password" ></TextInput>
+          </View>
         <View>
-        <View style={styles.AntDesign}> 
-          <AntDesign name="lock" size={30} color="blue"/>
-        </View>
-          <TextInput  secureTextEntry placeholder="Password" style={{borderWidth:1,marginTop:20, borderRadius:10,width:300,padding:10, textAlign:'center'}}></TextInput>
-        </View>
-        <View>
+          <View style={{marginLeft:0, marginTop: 20, }}>
+                <CheckBox style = {styles.checkbox}
+                checkedColor="red" title="Remember me"/>
+      
+            </View>
 
         </View>
       </View> 
       
-      <View style={{flexDirection: 'row', marginTop: 80}}> 
+      <View style={{flexDirection: 'row', marginTop: 200}}> 
         <Text style={{color: 'blue', fontWeight: 'bold',}}>Forgot Password?</Text>
       </View>
       <Text style={{marginTop: 20, backgroundColor: 'skyblue', padding: 10, width: 300, borderRadius: 20, textAlign: 'center'}}>Sign In</Text>
@@ -76,6 +78,8 @@ const styles = StyleSheet.create({
     marginTop: 30,
     position: 'absolute',
     left: 10,
+    border: 5,
+    borderRadius: 10
    
   },
   AntDesign:{
